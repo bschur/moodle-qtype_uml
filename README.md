bb7a8bb6a934aedfdccda4bae3bfae9f1ff5ae56
Aris Javet <16836170+arisjavet-personal@users.noreply.github.com>
2023-10-04 20:51:16 +0200

    feat(*): added basic question structure according to https://docs.moodle.org/dev/Question_types

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# moodle-uml
+# Moodle-UML (BFH)
+
+![](https://github.com/bschur/moodle-uml/actions/workflows/moodle-plugin-ci.yml/badge.svg)
+
+## What it is:
+
+A moodle plugin supporting a new type of question.
+
+## Installation:
+
+1. Extract the contents of the downloaded zip to `question/type/`.
+2. Rename the extracted folder to `moodle-uml`.
+3. Start the Moodle upgrade procedure.
+
+## Further information:
---
+++ lang/en/qtype_moodle-uml.php
@@ -0,0 +1,24 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['moodle-uml'] = 'Moodle-UML';
+$string['moodle-uml_help'] =
+        'Create a .. description of your question type'; // This is the text of the help icon show by the heading above the add/edit form when a question of this type is being edited.
+$string['moodle-uml_link'] = 'question/type/moodle-uml';
+$string['moodle-umladding'] = 'Adding a Moodle-UML question';
+$string['moodle-umlediting'] = 'Editing a Moodle-UML question';
+$string['moodle-umlsummary'] =
+        'A Moodle-UML question type which allows...'; // This is the text relating to this question type shown in the question-type chooser dialogue.
