64b4d7e7b66daa2714644eb546928952a28b9a7b
bschur <ron100.schuerch@gmail.com>
2023-11-01 14:04:15 +0100

    splitscreen with webgl

--- editor/webgl-editor.js
+++ editor/webgl-editor.js
@@ -4,20 +4,31 @@ main();
 // start here
 //
 function main() {
-  const canvas = document.querySelector("#glcanvas");
-  // Initialize the GL context
-  const gl = canvas.getContext("webgl");
+ // Holen Sie sich die Canvas-Elemente
+const canvas1 = document.getElementById('canvas1');
+const canvas2 = document.getElementById('canvas2');
+const gl1 = canvas1.getContext('webgl');
+const gl2 = canvas2.getContext('webgl');
 
-  // Only continue if WebGL is available and working
-  if (gl === null) {
-    alert(
-      "Unable to initialize WebGL. Your browser or machine may not support it.",
-    );
-    return;
-  }
+ // Only continue if WebGL is available and working
+ if (gl1 === null) {
+  alert(
+    "Unable to initialize WebGL. Your browser or machine may not support it.",
+  );
+  return;
+}
 
-  // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  // Clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
+// Zeichnen Sie etwas auf canvas1
+gl2.clearColor(0.0, 0.0, 0.0, 1.0);
+gl2.clear(gl2.COLOR_BUFFER_BIT);
+
+
+
+// Hier können Sie WebGL-Objekte auf canvas2 zeichnen
+
+
+
+ 
+
+
 }
