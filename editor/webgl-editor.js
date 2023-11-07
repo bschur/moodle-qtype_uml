b737094549f1bd5b65a22b6aa749bf6346b34e1f
Aris Javet <16836170+arisjavet-personal@users.noreply.github.com>
2023-11-07 15:31:28 +0100

    feat(webgl): refactor

--- editor/webgl-editor.js
+++ editor/webgl-editor.js
@@ -1,31 +1,25 @@
-const template = document.createElement('template');
-
-template.innerHTML = `
+const templateCode = `
 <style>
-:host {   
-    display: flex;
-    flex-direction: row;
-    height: 100%;
-    width: 100%;
-    gap: 2px;
-    padding: 4px;
-}
-
-.left {
-    width: 30%;
-    background-color: grey;
-}
-
-.right {
-    width: 100%;
-    background-color: grey;
-}
-
-canvas {
-    top: 0;
-}
+    :host {
+        display: flex;
+        flex-direction: row;
+        height: 100%;
+        width: 100%;
+        gap: 2px;
+        padding: 4px;
+    }
+    
+    .left {
+        width: 30%;
+        background-color: grey;
+    }
+    
+    .right {
+        width: 100%;
+        background-color: grey;
+    }
 </style>
-
+    
 <div class="left">
     <canvas id="canvasTool"></canvas>
 </div>
@@ -35,44 +29,49 @@ canvas {
 `
 
 class WebglEditor extends HTMLElement {
-    objects = [];
-
-    canvasEditor = null
-    ctxEditor = null
-
-    canvasTool = null
-    ctxTool = null
-
-    offsetX = 0
-    offsetY = 0
-    draggedObject = null;
-
     constructor() {
         super();
-        this._shadowRoot = this.attachShadow({'mode': 'open'});
-        this._shadowRoot.appendChild(template.content.cloneNode(true));
+        this.attachShadow({mode: 'open'});
+
+        // Create the template for the shadow DOM
+        this.shadowRoot.innerHTML = templateCode;
+
+        // Initialize properties
+        this.objects = [];
+        this.canvasEditor = null;
+        this.ctxEditor = null;
+        this.canvasTool = null;
+        this.ctxTool = null;
+        this.offsetX = 0;
+        this.offsetY = 0;
+        this.draggedObject = null;
     }
 
-    /**
-     * Is being called when the web-component is connected
-     */
     connectedCallback() {
-        const editorInformation = this.getCanvasInformation('canvasEditor');
-        this.canvasEditor = editorInformation.element
-        this.ctxEditor = editorInformation.context
+        // Initialize canvas elements and contexts
+        this.initCanvas('canvasEditor', 'ctxEditor');
+        this.initCanvas('canvasTool', 'ctxTool');
 
-        const toolInformation = this.getCanvasInformation('canvasTool')
-        this.canvasTool = toolInformation.element
-        this.ctxTool = toolInformation.context
+        // Setup event listeners
+        this.setupListeners();
 
-        this.setupListeners()
-        this.drawObjectTool()
+        // Initial rendering
+        this.drawObjectTool();
     }
 
-    /**
-     * Draw all instances in the Editor
-     */
+    initCanvas(canvasId, contextId) {
+        const canvas = this.shadowRoot.getElementById(canvasId);
+        const context = canvas.getContext('2d');
+
+        canvas.width = canvas.parentElement.clientWidth;
+        canvas.height = canvas.parentElement.clientHeight;
+
+        this[canvasId] = canvas;
+        this[contextId] = context;
+    }
+
     drawObjectEditor() {
+        // Clear the editor canvas and draw objects
         this.ctxEditor.clearRect(0, 0, this.canvasEditor.width, this.canvasEditor.height);
         for (const obj of this.objects) {
             this.ctxEditor.fillStyle = obj.color;
@@ -80,24 +79,18 @@ class WebglEditor extends HTMLElement {
         }
     }
 
-    /**
-     * Draw a instance for each object in the Toolbox
-     */
     drawObjectTool() {
+        // Clear the tool canvas and draw a sample object
         this.ctxTool.clearRect(0, 0, this.canvasTool.width, this.canvasTool.height);
-        this.ctxTool.fillStyle = "blue";
+        this.ctxTool.fillStyle = 'blue';
         this.ctxTool.fillRect(100, 100, 50, 50);
     }
 
     findTopObject(x, y) {
+        // Find the top object at the given coordinates
         for (let i = this.objects.length - 1; i >= 0; i--) {
             const obj = this.objects[i];
-            if (
-                x >= obj.x &&
-                x <= obj.x + obj.width &&
-                y >= obj.y &&
-                y <= obj.y + obj.height
-            ) {
+            if (x >= obj.x && x <= obj.x + obj.width && y >= obj.y && y <= obj.y + obj.height) {
                 return obj;
             }
         }
@@ -105,10 +98,8 @@ class WebglEditor extends HTMLElement {
     }
 
     setupListeners() {
-        /**
-         * Save dragged object
-         */
-        this.canvasEditor.addEventListener("mousedown", (event) => {
+        // Save dragged object
+        this.canvasEditor.addEventListener('mousedown', (event) => {
             const x = event.clientX - this.canvasEditor.getBoundingClientRect().left;
             const y = event.clientY - this.canvasEditor.getBoundingClientRect().top;
 
@@ -120,10 +111,8 @@ class WebglEditor extends HTMLElement {
             }
         });
 
-        /**
-         * refresh Editor for every mousemove
-         */
-        this.canvasEditor.addEventListener("mousemove", (event) => {
+        // Refresh Editor for every mousemove
+        this.canvasEditor.addEventListener('mousemove', (event) => {
             if (this.draggedObject) {
                 const x = event.clientX - this.canvasEditor.getBoundingClientRect().left;
                 const y = event.clientY - this.canvasEditor.getBoundingClientRect().top;
@@ -134,18 +123,16 @@ class WebglEditor extends HTMLElement {
             }
         });
 
-        /**
-         * save location of dragged object
-         */
-        this.canvasEditor.addEventListener("mouseup", () => {
-            this.objects[this.draggedObject.id] = this.draggedObject;
-            this.draggedObject = null;
+        // Save location of dragged object
+        this.canvasEditor.addEventListener('mouseup', () => {
+            if (this.draggedObject) {
+                this.objects[this.draggedObject.id - 1] = this.draggedObject;
+                this.draggedObject = null;
+            }
         });
 
-        /**
-         * Draw a instance of a clickt object (in Toolbox) on the editor
-         */
-        this.canvasTool.addEventListener("click", (event) => {
+        // Draw a instance of a clickt object (in Toolbox) on the editor
+        this.canvasTool.addEventListener('click', (event) => {
             event.preventDefault();
             const obj = {
                 id: this.objects.length + 1,
@@ -153,25 +140,12 @@ class WebglEditor extends HTMLElement {
                 y: 100,
                 width: 50,
                 height: 50,
-                color: "blue",
+                color: 'blue',
             };
             this.objects.push(obj);
             this.drawObjectEditor();
         });
     }
-
-    getCanvasInformation(id) {
-        const element = this._shadowRoot.getElementById(id);
-        const context = element.getContext("2d");
-
-        element.width = element.parentElement.clientWidth;
-        element.height = element.parentElement.clientHeight;
-
-        return {
-            element,
-            context
-        }
-    }
 }
 
-window.customElements.define('webgl-editor', WebglEditor);
+customElements.define('webgl-editor', WebglEditor);
