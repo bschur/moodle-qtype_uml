7b6d712adfd587a31f215614918b5212c0f17e50
bschur <ron100.schuerch@gmail.com>
2023-11-16 11:17:22 +0100

    cleanup

--- editor/webgl-editor.js
+++ editor/webgl-editor.js
@@ -48,39 +48,51 @@ class WebglEditor extends HTMLElement {
         // Create the JointJS diagram
         const editorDiv = this.shadowRoot.querySelector('#editor');
         const toolBoxDiv = this.shadowRoot.querySelector('#toolBox');
-
-        // Create an instance of JointJS graph for the editor and toolbox
+        
+        // Create  instance of  JointJS graph
         const graphEditor = new joint.dia.Graph();
         const graphToolBox = new joint.dia.Graph();
 
-        // Create a paper where the diagram will be rendered for the editor and toolbox
-        const paperEditor = new joint.dia.Paper({
-            el: editorDiv,
-            model: graphEditor,
-            width: '100%',
-            height: '100%',
-            gridSize: 10,
-            drawGrid: true,
-            background: {
-                color: 'rgba(0, 255, 255, 0.3)'
-            }
+        const paperEditor = initPaper(editorDiv, graphEditor, true);
+        const paperToolbox = initPaper(toolBoxDiv, graphToolBox, false);
+
+        const Class = initClass();
+        initToolBoxClasses();    
+
+        paperToolbox.on('element:pointerup', (cellView, evt, x, y) => {
+            const clickedClass = cellView.model.clone();
+            clickedClass.position(x, y);
+            graphEditor.addCell(clickedClass);
         });
 
-        const paperToolbox = new joint.dia.Paper({
-            el: toolBoxDiv,
-            model: graphToolBox,
-            width: '100%',
-            height: '100%',
-            gridSize: 10,
-            drawGrid: true,
-            interactive: false 
-        });
+        function initToolBoxClasses () {
+            // Create classes for the toolbox
+            const class1 = new Class({
+                position: { x: 50, y: 50 },
+                attrs: {
+                    label: { text: 'ClassA' }
+                },
+                draggable: false
+            });
 
-        // Define class shapes
+            const class2 = new Class({
+                position: { x: 50, y: 150 },
+                attrs: {
+                    label: { text: 'ClassB' }
+                }
+            });
+             // Add classes to the toolbox graph
+          graphToolBox.addCell(class1);
+          graphToolBox.addCell(class2);
+        }
+
+         
+
+
+    function initClass() {
         const rectWidth = 100;
         const rectHeight = 60;
-
-        const Class = joint.shapes.standard.Rectangle.define('examples.Class', {
+        return joint.shapes.standard.Rectangle.define('examples.Class', {
             size: { width: rectWidth, height: rectHeight },
             attrs: {
                 body: {
@@ -99,34 +111,20 @@ class WebglEditor extends HTMLElement {
                 }
             }
         });
-
-        // Create classes for the toolbox
-        const class1 = new Class({
-            position: { x: 50, y: 50 },
-            attrs: {
-                label: { text: 'ClassA' }
-            },
-            draggable: false
-        });
-
-        const class2 = new Class({
-            position: { x: 50, y: 150 },
-            attrs: {
-                label: { text: 'ClassB' }
-            }
-        });
-
-        // Add classes to the toolbox graph
-        graphToolBox.addCell(class1);
-        graphToolBox.addCell(class2);
-
-        paperToolbox.on('element:pointerup', (cellView, evt, x, y) => {
-            const clickedClass = cellView.model.clone();
-            clickedClass.position(x, y);
-            graphEditor.addCell(clickedClass);
-        });
-
     }
-}
+
+    function initPaper(el, model, isInteractive) {
+        return new joint.dia.Paper({
+            el: el,
+            model: model,
+            width: '100%',
+            height: '100%',
+            gridSize: 10,
+            drawGrid: true,
+            interactive: isInteractive 
+        });
+    }
+
+}}
 
 customElements.define('webgl-editor', WebglEditor);
