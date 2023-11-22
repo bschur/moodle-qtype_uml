e29535d874c8f95788900ba1b6faccdc17b213ca
schur11 <schur11@bfh.ch>
2023-11-22 10:45:48 +0100

    add basic Reletian

--- editor/webgl-editor.js
+++ editor/webgl-editor.js
@@ -1,130 +1,148 @@
-const templateCode = `
-<style>
-    :host {
-        display: flex;
-        flex-direction: row;
-        height: 100%;
-        width: 100%;
-        gap: 2px;
-        padding: 4px;
-    }
-    
-    .left {
-        width: 30%;
-        background-color: grey;
-    }
-    
-    .right {
-        width: 100%;
-    }
-
-    #editor {
-        width: 100%;
-        height: 100%;
-        border: 1px solid #ccc;
-    }
-</style>
-
-<div class="left">
-    <div id="toolBox"></div>
-</div>
-<div class="right">
-    <div id="editor"></div>
-</div>
-`;
-
-class WebglEditor extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-
-        // Create the template for the shadow DOM
-        this.shadowRoot.innerHTML = templateCode;
-
-        // Initialize properties
-        this.objects = [];
-        this.myholder = null;
-
-        // Create the JointJS diagram
-        const editorDiv = this.shadowRoot.querySelector('#editor');
-        const toolBoxDiv = this.shadowRoot.querySelector('#toolBox');
+    const templateCode = `
+    <style>
+        :host {
+            display: flex;
+            flex-direction: row;
+            height: 100%;
+            width: 100%;
+            gap: 2px;
+            padding: 4px;
+        }
         
-        // Create  instance of  JointJS graph
-        const graphEditor = new joint.dia.Graph();
-        const graphToolBox = new joint.dia.Graph();
+        .left {
+            width: 30%;
+            background-color: grey;
+        }
+        
+        .right {
+            width: 100%;
+        }
+    
+        #editor {
+            width: 100%;
+            height: 100%;
+            border: 1px solid #ccc;
+        }
+    </style>
+    
+    <div class="left">
+        <div id="toolBox"></div>
+    </div>
+    <div class="right">
+        <div id="editor"></div>
+    </div>
+    `;
 
-        const paperEditor = initPaper(editorDiv, graphEditor, true);
-        const paperToolbox = initPaper(toolBoxDiv, graphToolBox, false);
+    class WebglEditor extends HTMLElement {
+        constructor() {
+            super();
+            this.attachShadow({ mode: 'open' });
 
-        const Class = initClass();
-        initToolBoxClasses();    
+            // Create the template for the shadow DOM
+            this.shadowRoot.innerHTML = templateCode;
 
-        paperToolbox.on('element:pointerup', (cellView, evt, x, y) => {
-            const clickedClass = cellView.model.clone();
-            clickedClass.position(x, y);
-            graphEditor.addCell(clickedClass);
-        });
+            // Initialize properties
+            this.objects = [];
+            this.myholder = null;
 
-        function initToolBoxClasses () {
-            // Create classes for the toolbox
-            const class1 = new Class({
-                position: { x: 50, y: 50 },
-                attrs: {
-                    label: { text: 'ClassA' }
-                },
-                draggable: false
+            // Create the JointJS diagram
+            const editorDiv = this.shadowRoot.querySelector('#editor');
+            const toolBoxDiv = this.shadowRoot.querySelector('#toolBox');
+
+            // Create  instance of  JointJS graph
+            const graphEditor = new joint.dia.Graph();
+            const graphToolBox = new joint.dia.Graph();
+
+            const paperEditor = initPaper(editorDiv, graphEditor, true);
+            const paperToolbox = initPaper(toolBoxDiv, graphToolBox, false);
+
+            const Class = initClass();
+            initToolBoxClasses();
+
+            paperToolbox.on('element:pointerup', (cellView, evt, x, y) => {
+                const clickedClass = cellView.model.clone();
+                clickedClass.position(x, y);
+                graphEditor.addCell(clickedClass);
             });
 
-            const class2 = new Class({
-                position: { x: 50, y: 150 },
-                attrs: {
-                    label: { text: 'ClassB' }
+            paperEditor.on('cell:pointerup', (cellView, evt, x, y) => {
+                if (this.from) {
+                    // If 'from' is set (meaning a previous element was selected), create a link
+                    const link = new joint.shapes.standard.Link({
+                        source: { id: this.from.id },
+                        target: { id: cellView.model.id },
+                        attrs: {
+                            // Define link styles here if needed
+                        }
+                    });
+                    graphEditor.addCell(link);
+                    this.from = null; // Reset 'from' to enable selecting a new 'from' element
+                } else {
+                    // Set the 'from' element upon the first click
+                    this.from = cellView.model;
                 }
             });
-             // Add classes to the toolbox graph
-          graphToolBox.addCell(class1);
-          graphToolBox.addCell(class2);
+
+            function initToolBoxClasses () {
+                // Create classes for the toolbox
+                const class1 = new Class({
+                    position: { x: 50, y: 50 },
+                    attrs: {
+                        label: { text: 'ClassA' }
+                    },
+                    draggable: false
+                });
+
+                const class2 = new Class({
+                    position: { x: 50, y: 150 },
+                    attrs: {
+                        label: { text: 'ClassB' }
+                    }
+                });
+                 // Add classes to the toolbox graph
+              graphToolBox.addCell(class1);
+              graphToolBox.addCell(class2);
+            }
+
+
+
+
+        function initClass() {
+            const rectWidth = 100;
+            const rectHeight = 60;
+            return joint.shapes.standard.Rectangle.define('examples.Class', {
+                size: { width: rectWidth, height: rectHeight },
+                attrs: {
+                    body: {
+                        fill: '#2ECC71',
+                        rx: 5,
+                        ry: 5,
+                        strokeWidth: 2,
+                        stroke: 'black'
+                    },
+                    label: {
+                        text: 'ClassName',
+                        fill: 'black',
+                        fontSize: 12,
+                        fontWeight: 'bold',
+                        fontFamily: 'Arial, helvetica, sans-serif'
+                    }
+                }
+            });
         }
 
-         
+        function initPaper(el, model, isInteractive) {
+            return new joint.dia.Paper({
+                el: el,
+                model: model,
+                width: '100%',
+                height: '100%',
+                gridSize: 10,
+                drawGrid: true,
+                interactive: isInteractive
+            });
+        }
 
+    }}
 
-    function initClass() {
-        const rectWidth = 100;
-        const rectHeight = 60;
-        return joint.shapes.standard.Rectangle.define('examples.Class', {
-            size: { width: rectWidth, height: rectHeight },
-            attrs: {
-                body: {
-                    fill: '#2ECC71',
-                    rx: 5,
-                    ry: 5,
-                    strokeWidth: 2,
-                    stroke: 'black'
-                },
-                label: {
-                    text: 'ClassName',
-                    fill: 'black',
-                    fontSize: 12,
-                    fontWeight: 'bold',
-                    fontFamily: 'Arial, helvetica, sans-serif'
-                }
-            }
-        });
-    }
-
-    function initPaper(el, model, isInteractive) {
-        return new joint.dia.Paper({
-            el: el,
-            model: model,
-            width: '100%',
-            height: '100%',
-            gridSize: 10,
-            drawGrid: true,
-            interactive: isInteractive 
-        });
-    }
-
-}}
-
-customElements.define('webgl-editor', WebglEditor);
+    customElements.define('webgl-editor', WebglEditor);
