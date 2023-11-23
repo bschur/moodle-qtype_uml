393dccef1dfc489d09e463f4f3ae718fb801d944
Luca Bösch <luca.boesch@bfh.ch>
2023-11-23 01:32:08 +0100

    Basic initial implementation of the PHPUnit classes fixes #15.

--- question.php
+++ question.php
@@ -27,6 +27,24 @@
  */
 class qtype_uml_question extends question_graded_automatically_with_countback {
 
+    // Definition of properties used in legacy code or tests, for compatibility with PHP 8.2.
+    /** @var string Feedback for any correct response. */
+    public $correctfeedback;
+    /** @var int format of $correctfeedback. */
+    public $correctfeedbackformat;
+    /** @var string Feedback for any partially correct response. */
+    public $partiallycorrectfeedback;
+    /** @var int format of $partiallycorrectfeedback. */
+    public $partiallycorrectfeedbackformat;
+    /** @var string Feedback for any incorrect response. */
+    public $incorrectfeedback;
+    /** @var int format of $incorrectfeedback. */
+    public $incorrectfeedbackformat;
+    /** @var string Information for the graders. */
+    public $graderinfo;
+    /** @var int format of $graderinfo. */
+    public $graderinfoformat;
+
     /**
      * Reads the expected data
      */
