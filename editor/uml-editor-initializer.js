5c1a41c48e6ac082cb163e1933dde42ca762df0b
Aris Javet <16836170+arisjavet-personal@users.noreply.github.com>
2023-11-30 16:48:30 +0100

    feat(editor-build): change styling and recursive copying of folder structure

--- editor/uml-editor-initializer.js
+++ editor/uml-editor-initializer.js
@@ -13,8 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
-import {UmlEditor} from './uml-editor.js';
-import {diagramChangeEventName, setDiagramToReferenceInputField} from './uml-editor-change-handler.js';
+import { UmlEditor } from './uml-editor.js';
+import { diagramChangeEventName, setDiagramToReferenceInputField } from './uml-editor-change-handler.js';
 
 // Setup web-component
 customElements.define('uml-editor', UmlEditor);
