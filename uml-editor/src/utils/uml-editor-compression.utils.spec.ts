eec671cfd1c5a04bc049987157b16eb52cbe25a0
Aris Javet <16836170+arisjavet-personal@users.noreply.github.com>
2024-03-07 10:04:39 +0100

    fix(ci): specify path to package-lock.json

--- uml-editor/src/utils/uml-editor-compression.utils.spec.ts
+++ uml-editor/src/utils/uml-editor-compression.utils.spec.ts
@@ -2,4 +2,8 @@ describe('UmlEditorCompressionUtils Tests', () => {
   it('Positive Test for checking if Framework runs', () => {
     expect(true).toBeTruthy()
   })
+
+  it('Negative Test for Pipeline', () => {
+    expect(false).toBeTruthy()
+  })
 })
