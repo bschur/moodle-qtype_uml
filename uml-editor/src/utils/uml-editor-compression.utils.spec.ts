a9049d61a4a1721a0901ca23c56452363d3603ba
Aris Javet <16836170+arisjavet-personal@users.noreply.github.com>
2024-03-07 09:58:19 +0100

    feat(ci): added angular test and lint to ci

---
+++ uml-editor/src/utils/uml-editor-compression.utils.spec.ts
@@ -0,0 +1,9 @@
+describe('UmlEditorCompressionUtils Tests', () => {
+  it('Positive Test for checking if Framework runs', () => {
+    expect(true).toBeTruthy()
+  })
+
+  it('Negative Test for Pipeline', () => {
+    expect(false).toBeTruthy()
+  })
+})
