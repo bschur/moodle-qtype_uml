1ddf1ed4dc65ef15a12c8d225f9c4e5c86f7bcd2
Aris Javet <16836170+arisjavet-personal@users.noreply.github.com>
2024-03-07 10:00:26 +0100

    fix(ci): removed test for checking if pipeline fails

--- uml-editor/src/utils/uml-editor-compression.utils.spec.ts
+++ uml-editor/src/utils/uml-editor-compression.utils.spec.ts
@@ -2,8 +2,4 @@ describe('UmlEditorCompressionUtils Tests', () => {
   it('Positive Test for checking if Framework runs', () => {
     expect(true).toBeTruthy()
   })
-
-  it('Negative Test for Pipeline', () => {
-    expect(false).toBeTruthy()
-  })
 })
