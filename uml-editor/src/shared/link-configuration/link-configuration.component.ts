3292b7a3460f85bcbe9797b31d5c40270b536995
Aris Javet <16836170+arisjavet-personal@users.noreply.github.com>
2024-03-28 15:11:10 +0100

    feat(link-configuration): added first ui elements

--- uml-editor/src/shared/link-configuration/link-configuration.component.ts
+++ uml-editor/src/shared/link-configuration/link-configuration.component.ts
@@ -1,14 +1,26 @@
-import { JsonPipe } from '@angular/common'
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { MatButton } from '@angular/material/button'
+import { MatFormField, MatLabel } from '@angular/material/form-field'
+import { MatIcon } from '@angular/material/icon'
+import { MatOption, MatSelect } from '@angular/material/select'
 import { dia } from '@joint/core'
 
 @Component({
   standalone: true,
-  imports: [JsonPipe],
+  imports: [MatButton, MatIcon, MatLabel, MatFormField, MatSelect, MatOption],
   templateUrl: './link-configuration.component.html',
   styleUrl: './link-configuration.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LinkConfigurationComponent {
   @Input({ required: true }) model!: dia.Link
+
+  readonly arrows = ['none', 'source', 'target', 'both']
+  readonly lines = ['normal', 'dotted']
+
+  swapDirection() {
+    const source = this.model.prop('source')
+    this.model.prop('source', this.model.prop('target'))
+    this.model.prop('target', source)
+  }
 }
