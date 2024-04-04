283aa1abead0ab90fa6a7b4581f8b0585791812b
Aris Javet <16836170+arisjavet-personal@users.noreply.github.com>
2024-04-04 13:24:10 +0200

    feat(correction-utils): use typing

--- uml-editor/src/utils/correction.utils.ts
+++ uml-editor/src/utils/correction.utils.ts
@@ -15,36 +15,63 @@ const ignoredProperties = [
   'd',
   'fill',
   'anchor',
+  'vertices',
   'strokeDasharray',
   'strokeDashoffset',
 ]
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const cleanupObjectProperties = (acc: any, [key, value]: [string, any]): any => {
+type PropertyValueType =
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | PropertyValueType[]
+  | { [key: string]: PropertyValueType }
+
+const cleanupProperty = ([key, value]: [string, PropertyValueType]): [string, PropertyValueType] | null => {
   if (value === null || value === undefined || ignoredProperties.includes(key)) {
-    return acc
+    return null
   }
 
-  if (typeof value === 'object') {
-    const cleaned = Object.entries(value).reduce(cleanupObjectProperties, {})
-    if (Object.keys(cleaned).length > 0) {
-      acc[key] = cleaned
-    }
-    return acc
+  // if the value is not an object, we can return the key value pair
+  if (typeof value !== 'object') {
+    return [key, value]
   }
 
+  // handle arrays
   if (Array.isArray(value)) {
-    acc[key] = Array.from(value).reduce(cleanupObjectProperties)
-    return acc
+    if (value.length === 0) {
+      return null
+    }
+
+    return [key, value.map((value, ix) => cleanupProperty([ix.toString(), value])?.[1] || null).filter(val => !!val)]
   }
 
-  acc[key] = value
-  return acc
+  // otherwise we have an object
+  if (Object.keys(value).length === 0) {
+    // if the object is empty, we can remove it
+    return null
+  }
+
+  const cleaned = Object.fromEntries(
+    Object.entries(value)
+      .map(cleanupProperty)
+      .filter((entry): entry is [string, PropertyValueType] => !!entry)
+  )
+  if (Object.keys(cleaned).length > 0) {
+    return [key, cleaned]
+  }
+  return null
 }
 
 function cleanupDiagram(diagram: JointJSDiagram): JointJSDiagram {
   // remove metadata on diagram level by just returning the cells
-  return Object.entries(diagram).reduce(cleanupObjectProperties, {})
+  return Object.fromEntries(
+    Object.entries(diagram)
+      .map(cleanupProperty)
+      .filter((entry): entry is [string, PropertyValueType] => !!entry)
+  ) as unknown as JointJSDiagram
 }
 
 function calculatePoints(referenceDiagram: JointJSDiagram, differences: JustDiff[], maxPoints: number): number {
