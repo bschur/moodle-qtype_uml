f7842df1c56861794301a43b3094fe64575e4582
bschur <ron100.schuerch@gmail.com>
2024-04-24 18:52:59 +0200

    64 fix resize use case (#67)
    * refactor ellipse inclusive resize
    
    * fix textbox
    
    ---------
    
    Co-authored-by: schur11 <schur11@bfh.ch>

--- uml-editor/src/models/jointjs/uml-use-case.model.ts
+++ uml-editor/src/models/jointjs/uml-use-case.model.ts
@@ -3,11 +3,18 @@ import { UseCaseConfigurationComponent } from '../../shared/use-case-configurati
 import { CustomJointJSElementAttributes } from './custom-jointjs-element.model'
 import { TextBlock } from './text-block.model'
 
-export class UseCase extends shapes.standard.Ellipse {
+let width = 150
+let heigth = 100
+let isTextBox = false
+export class UseCase extends shapes.standard.Rectangle {
   override readonly markup = [
     {
-      tagName: 'ellipse',
+      tagName: 'rect',
       selector: 'body',
+    },
+    {
+      tagName: 'ellipse',
+      selector: 'ellipse',
       children: [
         {
           tagName: 'textBox',
@@ -17,29 +24,66 @@ export class UseCase extends shapes.standard.Ellipse {
     },
   ]
 
-  userInput() {
-    const ctb = new TextBlock()
-
-    ctb.position().x = this.position().x
-    ctb.position().y = this.position().y + (this.size().width - 5) / 4
-    ctb.size().width = this.size().width - 5
-    ctb.size().height = this.size().height / 2
-
-    // Alternatively, store the textbox reference for further manipulation
-    this.embed(ctb)
-    return ctb
-  }
-
   override defaults() {
-    const elementAttributes: CustomJointJSElementAttributes<shapes.standard.EllipseAttributes> = {
+    const elementAttributes: CustomJointJSElementAttributes<shapes.standard.RectangleAttributes> = {
       type: 'custom.uml.UseCase',
       propertyView: UseCaseConfigurationComponent,
       size: {
-        width: 95,
-        height: 45,
+        width: width,
+        height: heigth,
+      },
+      attrs: {
+        body: {
+          opacity: 0,
+        },
+        ['ellipse']: {
+          width: '100%',
+          height: '100%',
+          stroke: 'black',
+          strokeWidth: 3,
+          rx: width / 2,
+          ry: heigth / 2,
+          'ref-y': 0.5,
+          'ref-x': 0.5,
+          'ref-Cy': 0.5,
+          'ref-Cx': 0.5,
+          ref: 'body',
+          fill: 'white',
+        },
       },
     }
     util.defaultsDeep(elementAttributes, super.defaults)
     return elementAttributes
+  }
+
+  userInput() {
+    if (!isTextBox) {
+      const ctb = new TextBlock()
+
+      ctb.size(this.size().width - 5, this.size().height / 2)
+      const tbWidth = this.position().x + width / 2 - ctb.size().width / 2
+      const tbHeight = this.position().y + heigth / 2 - ctb.size().height / 2
+      ctb.position(tbWidth, tbHeight)
+
+      ctb.attr('ref', 'ellipse')
+
+      isTextBox = true
+      this.embed(ctb)
+      return ctb
+    }
+    return null
+  }
+
+  override resize(widthNew: number, heightNew: number) {
+    super.resize(widthNew, heightNew)
+    heigth = heightNew
+    width = widthNew
+
+    this.attr('ellipse', {
+      rx: width / 2,
+      ry: heigth / 2,
+    })
+
+    return this
   }
 }
