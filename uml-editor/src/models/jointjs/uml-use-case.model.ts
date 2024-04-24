67b26ef2577a5242216766b5c2d00a320b4650ee
schur11 <schur11@bfh.ch>
2024-04-24 13:16:56 +0200

    refactor ellipse inclusive resize

--- uml-editor/src/models/jointjs/uml-use-case.model.ts
+++ uml-editor/src/models/jointjs/uml-use-case.model.ts
@@ -3,11 +3,17 @@ import { UseCaseConfigurationComponent } from '../../shared/use-case-configurati
 import { CustomJointJSElementAttributes } from './custom-jointjs-element.model'
 import { TextBlock } from './text-block.model'
 
-export class UseCase extends shapes.standard.Ellipse {
+let width = 150
+let heigth = 100
+export class UseCase extends shapes.standard.Rectangle {
   override readonly markup = [
     {
-      tagName: 'ellipse',
+      tagName: 'rect',
       selector: 'body',
+    },
+    {
+      tagName: 'ellipse',
+      selector: 'ellipse',
       children: [
         {
           tagName: 'textBox',
@@ -16,6 +22,40 @@ export class UseCase extends shapes.standard.Ellipse {
       ],
     },
   ]
+
+  override defaults() {
+    const elementAttributes: CustomJointJSElementAttributes<shapes.standard.RectangleAttributes> = {
+      type: 'custom.uml.UseCase',
+      propertyView: UseCaseConfigurationComponent,
+      size: {
+        width: width,
+        height: heigth,
+      },
+      attrs: {
+        body: {
+          //opacity: '100%',
+          strokeWidth: 4,
+          stroke: 'black',
+        },
+        ['ellipse']: {
+          width: '100%',
+          height: '100%',
+          stroke: 'black',
+          strokeWidth: 3,
+          rx: width / 2,
+          ry: heigth / 2,
+          'ref-y': 0.5,
+          'ref-x': 0.5,
+          'ref-Cy': 0.5,
+          'ref-Cx': 0.5,
+          ref: 'body',
+          fill: 'red',
+        },
+      },
+    }
+    util.defaultsDeep(elementAttributes, super.defaults)
+    return elementAttributes
+  }
 
   userInput() {
     const ctb = new TextBlock()
@@ -30,16 +70,16 @@ export class UseCase extends shapes.standard.Ellipse {
     return ctb
   }
 
-  override defaults() {
-    const elementAttributes: CustomJointJSElementAttributes<shapes.standard.EllipseAttributes> = {
-      type: 'custom.uml.UseCase',
-      propertyView: UseCaseConfigurationComponent,
-      size: {
-        width: 95,
-        height: 45,
-      },
-    }
-    util.defaultsDeep(elementAttributes, super.defaults)
-    return elementAttributes
+  override resize(widthNew: number, heightNew: number) {
+    super.resize(widthNew, heightNew)
+    heigth = heightNew
+    width = widthNew
+
+    this.attr('ellipse', {
+      rx: width / 2,
+      ry: heigth / 2,
+    })
+
+    return this
   }
 }
