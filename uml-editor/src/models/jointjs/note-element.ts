542d8a90a71349e8fa3184f862813c59d99bac9d
bschur <ron100.schuerch@gmail.com>
2024-05-02 11:50:43 +0200

    74 note element (#77)
    * refactor
    
    * details
    
    * mini changes
    
    * fix bug
    
    * feat(cleanup): pr improvements
    
    ---------
    
    Co-authored-by: schur11 <schur11@bfh.ch>
    Co-authored-by: Aris Javet <16836170+arisjavet-personal@users.noreply.github.com>

---
+++ uml-editor/src/models/jointjs/note-element.ts
@@ -0,0 +1,123 @@
+import { dia, shapes, util } from '@joint/core'
+import { CustomJointJSElementAttributes } from './custom-jointjs-element.model'
+
+export const NoteElementView = dia.ElementView.extend({
+  events: {
+    'input input': 'onInput',
+  },
+  onInput: function (event: dia.Event) {
+    this.model.attr('text/props/value', event.target.value)
+  },
+})
+
+const initialWidth = 150
+const initialHeight = 80
+const initBackgroundColor = '#f5e487'
+
+const markup = [
+  {
+    tagName: 'rect',
+    selector: 'body',
+  },
+  {
+    tagName: 'rect',
+    selector: 'header',
+  },
+  {
+    tagName: 'text',
+    selector: 'title',
+  },
+  {
+    tagName: 'foreignObject',
+    selector: 'foreignObject',
+    children: [
+      {
+        tagName: 'div',
+        namespaceURI: 'http://www.w3.org/1999/xhtml',
+        selector: 'background',
+        style: {
+          width: '100%', // Adjust the width as needed
+          height: '100%', // Adjust the height as needed
+        },
+        children: [
+          {
+            tagName: 'textarea',
+            selector: 'input',
+            style: {
+              backgroundColor: initBackgroundColor,
+              font: '14px sans-serif',
+              padding: '2px',
+              margin: 0,
+              resize: 'none', // Prevent resizing if desired
+              width: '100%', // Adjust the width as needed
+              height: '100%', // Adjust the height as needed
+              //border: '1px solid black', // Optional border styling
+            },
+          },
+        ],
+      },
+    ],
+  },
+]
+
+export class NoteElement extends shapes.standard.Rectangle {
+  override readonly markup = markup
+
+  // Override the defaults if necessary
+  override defaults() {
+    const elementAttributes: CustomJointJSElementAttributes<dia.Element.Attributes> = {
+      type: 'custom.uml.NoteElement',
+      size: {
+        width: initialWidth,
+        height: initialHeight,
+      },
+      attrs: {
+        body: {
+          rx: 0,
+          ry: 0,
+          strokeWidth: 4,
+          stroke: 'black',
+          fill: initBackgroundColor,
+        },
+        header: {
+          ref: 'body',
+          x: 0,
+          y: 0,
+          width: initialWidth,
+          height: 20,
+          fill: '#f5e487',
+        },
+        title: {
+          text: 'Note',
+          width: '100%',
+          height: 20,
+          fontWeight: 'bold',
+          'ref-y': 0,
+          'ref-x': 0.5, // Adjust reference to center horizontally
+          'text-anchor': 'middle', // Center align text horizontally
+          ref: 'body',
+        },
+        foreignObject: {
+          width: initialWidth,
+          height: initialHeight - 20,
+          x: 0,
+          y: 20,
+          ref: 'body',
+        },
+      },
+      text: '',
+    }
+
+    util.defaultsDeep(elementAttributes, super.defaults)
+    return elementAttributes
+  }
+
+  override resize(width: number, height: number) {
+    this.attr('body/width', width)
+    this.attr('header/width', width)
+    this.attr('foreignObject/width', width)
+    this.attr('foreignObject/height', height - 20)
+    super.resize(width, height)
+    return this
+  }
+}
--- uml-editor/src/utils/jointjs-extension.const.ts
+++ uml-editor/src/utils/jointjs-extension.const.ts
@@ -1,4 +1,5 @@
 import { CustomJointJSElement, CustomJointJSElementView } from '../models/jointjs/custom-jointjs-element.model'
+import { NoteElement, NoteElementView } from '../models/jointjs/note-element'
 import { TextBlock, TextBlockView } from '../models/jointjs/text-block.model'
 import { UmlActor } from '../models/jointjs/uml-actor.model'
 import { UmlClass } from '../models/jointjs/uml-classifier/uml-class.model'
@@ -12,10 +13,12 @@ export const jointJSCustomUmlElements: CustomJointJSElement[] = [
   createCustomJointJSElement(UmlClass, 'Classifier', true),
   createCustomJointJSElement(TextBlock, 'Text-block', false),
   createCustomJointJSElement(UseCase, 'UseCase', true),
-  createCustomJointJSElement(UmlInterface, 'Interface', true),
-  createCustomJointJSElement(UmlEnum, 'Enum', true),
+  createCustomJointJSElement(UmlInterface, 'Interface', false),
+  createCustomJointJSElement(UmlEnum, 'Enum', false),
+  createCustomJointJSElement(NoteElement, 'NoteElement', true),
 ]
 
 export const jointJSCustomUmlElementViews: CustomJointJSElementView[] = [
   createCustomJointJSElementView(TextBlockView, 'custom.uml.TextBlockView'),
+  createCustomJointJSElementView(NoteElementView, 'custom.uml.NoteElementView'),
 ]
