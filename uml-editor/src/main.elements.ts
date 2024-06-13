477d100cd783678043db95ee08af997540d8808f
Aris Javet <16836170+arisjavet-personal@users.noreply.github.com>
2024-06-13 13:37:24 +0200

    fix(build): readed zone.js for working inside moodle

--- uml-editor/src/main.elements.ts
+++ uml-editor/src/main.elements.ts
@@ -1,4 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser'
+import 'zone.js'
 import { appConfig } from './app/app.config'
 import { ElementsComponent } from './elements/elements.component'
 
